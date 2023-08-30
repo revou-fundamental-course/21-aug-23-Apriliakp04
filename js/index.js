20f3a7cad6b932b6dbabc84d458b3fb13a589ad0
Apriliakp04 <apriliakhadijahp@example.com>
2023-08-31 04:31:23 +0700

    lat

--- js/index.js
+++ js/index.js
@@ -2,14 +2,4 @@ let nameInput= document.getElementById('name-input')
 document.getElementById('submit').addEventListener("click", function() {
     const nama = nameInput.value;
     document.getElementById('name').innerHTML = nama;})
-
-    function hasil(){
-
-        var nama = document.getElementById("name-input").value;
-        var born= document.getElementById("lahir").value;
-        var kelamin = document.getElementById("pria/wanita").value;
-        var message= document.getElementById("pesan1").value;
-
-        alert("Nama :"+ nama , "Tanggal Lahir:" + born, "Jenis Kelamin:" + kelamin, "Pesan :"+ message);
-    }
     

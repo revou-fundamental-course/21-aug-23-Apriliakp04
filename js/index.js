e4cde618c99d1ba732557087f429b688e96f8d40
Apriliakp04 <apriliakhadijahp@example.com>
2023-08-31 18:03:05 +0700

    upppp

--- js/index.js
+++ js/index.js
@@ -3,3 +3,15 @@ document.getElementById('submit').addEventListener("click", function() {
     const nama = nameInput.value;
     document.getElementById('name').innerHTML = nama;})
     
+
+    function submitdata(){
+
+        var nameInput =document.getElementById("name-input").value;
+        var  lahir =document.getElementById("lahir").value;
+        var gender =document.getElementById("gender").value;
+        var pesan1 =document.getElementById("pesan1").value;
+
+    alert (" Nama : " + nameInput + "\n Tanggal Lahir :"+  lahir + 
+    "\n Jenis Kelamin : "+  gender +"\n Pesan : " + pesan1);
+        
+    }
